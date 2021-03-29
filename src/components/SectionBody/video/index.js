a51d67780c5d10cac854b5144d72cf1f282bbbbd
Vince <vince.qmarketz@gmail.com>
2021-03-30 05:18:44 +0800

    changed to appropriate HowItWorks video

--- src/components/SectionBody/video/index.js
+++ src/components/SectionBody/video/index.js
@@ -26,11 +26,12 @@ const video = () => {
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/tSM8gkFdTIA"
-        frameBorder="0"
+        src="https://www.youtube.com/embed/O1b4LEQdfQg"
+        title="YouTube video player"
+        frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      ></iframe>
+      />
     </Paper>
   );
 };
